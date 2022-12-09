fdaf9db9937685375541192a093861ca1b2ed14f
Jonathan Kolb <joni.k.spiegel@googlemail.com>
2022-12-09 13:24:28 +0000

    Test Component to try out conditional rendering

---
+++ src/TestComp.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import React, { Component } from "react";
+
+class TestComp extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Test Text</p>
+      </div>
+    );
+  }
+}
+
+export default TestComp;
